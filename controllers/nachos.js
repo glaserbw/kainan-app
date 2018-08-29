4374013a8448bd80a0b42b2edb0c45bcf99d3dab
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-29 14:35:23 -0700

    overcame dead end. still working on parsing my URL and displaying results

--- controllers/nachos.js
+++ controllers/nachos.js
@@ -11,15 +11,21 @@ router.post('/', loggedIn, function(req, res){
 		var searchQuery = req.body.search;
 		// console.log(searchQuery);
 		var searchUrl = 'https://developers.zomato.com/api/v2.1/locations?query=' + searchQuery + '&apikey=' + process.env.API_KEY;  
-		// console.log(searchUrl);
-
+		var cityId; 
 	request(searchUrl, function(error, response, body) {
-			console.log('body is: ', body)
-
+		// console.log('body is: ', body);
+		var parsedJson = JSON.parse(body); 
+		// console.log(parsedJson);
+		cityId = parsedJson.location_suggestions[0].city_id;
+		// console.log('city id is: ', cityId);
+		var resultUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=' + cityId + '&entity_type=city&q=dish-nacho' + '&apikey=' + process.env.API_KEY;
+		// console.log('resultUrl: ', resultUrl);
 		res.render('nachos/index')
 	}); 
 }); 
 
 
 module.exports = router; 
+
+
 
