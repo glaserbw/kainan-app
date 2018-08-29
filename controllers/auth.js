5358faf4e04c6ed1ec1b74a4907ba535359be39a
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-28 17:58:53 -0700

    added nachos view and controller. can click through to index but not with search results

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ router.get('/signup', function(req, res){
 }); 
 
 router.post('/signup', function(req, res){
-	console.log(req.body);
+	// console.log(req.body);
 	req.body.admin = false; 
 	db.user.findOrCreate({
 		where: { email: req.body.email },
--- controllers/nachos.js
+++ controllers/nachos.js
@@ -9,6 +9,8 @@ var loggedIn = require('../middleware/loggedIn');
 // Define routes
 router.get('/', loggedIn, function(req, res){
 	// API CALL NEEDS TO GO HERE
+	console.log('search is: ', req.body)
+
 	res.render('nachos/index');
 }); 
 
