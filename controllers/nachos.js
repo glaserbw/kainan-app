7ba0f94849cccda6b8e58c07fe3f09ffe625073b
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-29 10:46:59 -0700

    search is returning results. need to fine tune to deliver expected results

--- controllers/nachos.js
+++ controllers/nachos.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var router = express.Router();
 var db = require('../models');
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
 
 
 // Get the authorization helper function
 var loggedIn = require('../middleware/loggedIn'); 
 
 // Define routes
-router.get('/', loggedIn, function(req, res){
-	// API CALL NEEDS TO GO HERE
-	console.log('search is: ', req.body)
+router.post('/', loggedIn, function(req, res){
+		var searchQuery = req.body.search;
+		// console.log(searchQuery);
+		var searchUrl = 'https://developers.zomato.com/api/v2.1/locations?query=' + searchQuery + '&apikey=' + process.env.API_KEY;  
+		// console.log(searchUrl);
 
-	res.render('nachos/index');
+	request(searchUrl, function(error, response, body) {
+			console.log('body is: ', body)
+
+	// 	res.render('nachos/index')
+	// 	// console.log('search is: ', req.body)
+	}); 
 }); 
 
 
