b2d901a144a7e0430ffb1e0e6c50da35747a3695
brandiw <brandimichelle.0911@gmail.com>
2018-08-30 08:56:28 -0700

    Always have the localest variables you can! Think about hundreds of people accessing a website simultaneously and declaring anything that isn't more or less constant no longer makes sense.

--- controllers/nachos.js
+++ controllers/nachos.js
@@ -4,44 +4,30 @@ var request = require('request');
 var router = express.Router();
 
 // Get the authorization helper function
-var loggedIn = require('../middleware/loggedIn'); 
-
-var restaurantList; 
-var dataToPass;
+var loggedIn = require('../middleware/loggedIn');
 
 // Define routes
 router.post('/', loggedIn, function(req, res){
-		var searchQuery = req.body.search;
-		// console.log(searchQuery);
-		var searchUrl = 'https://developers.zomato.com/api/v2.1/locations?query=' + searchQuery + '&apikey=' + process.env.API_KEY;  	
+  // Accept: alphanumeric, underscore, comma, whitespace
+	var searchQuery = req.body.search.replace(/[^\w\,\s]/g, '');
+	var searchUrl = 'https://developers.zomato.com/api/v2.1/locations?query=' + searchQuery + '&apikey=' + process.env.API_KEY;
 
 	request(searchUrl, function(error, response, body) {
-		// parse the response from search URL into a readable form
-		var parsedJson = JSON.parse(body); 
-		// take parsed date and look for city_id integer for next step 
-		var cityId = parsedJson.location_suggestions[0].city_id;
-		// Invoke the cityId function created for getting Nacho listings 
-		citySearch(cityId);
-	})
-	res.render('nachos/index', dataToPass);
+		// look for city_id integer for next step
+		var cityId = JSON.parse(body).location_suggestions[0].city_id;
+    var resultUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=' + cityId + '&entity_type=city&cuisines=112' + '&apikey=' + process.env.API_KEY;
+    request(resultUrl, function(error, response, body){
+      var restaurantList = JSON.parse(body);
+      res.render('nachos/index', {
+        restaurants: restaurantList.restaurants,
+        name: "Taylor" // ex. here to remind me of value pair relationships
+      });
+    });
+	});
 });
 
-// this function takes the users location search and reqests Nacho specific restaurant listings using their cityId
-var citySearch = function(cityId){
-	var resultUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=' + cityId + '&entity_type=city&cuisines=112' + '&apikey=' + process.env.API_KEY;
-	console.log(resultUrl);
-	request(resultUrl, function(error, response, body){
-		restaurantList = JSON.parse(body); 
-		console.log(restaurantList.restaurants[0].restaurant.name);
-		dataToPass = {
-			restaurants: restaurantList.restaurants,
-			name: "Taylor" // ex. here to remind me of value pair relationships 
-		}
-	});
-} 
 
-
-module.exports = router; 
+module.exports = router;
 
 // &q=dish-nachos added in URL before api key in citySearch function
 
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// Require needed modules 
+// Require needed modules
 require('dotenv').config();
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
-var express = require('express'); 
+var express = require('express');
 var flash = require('connect-flash');
-var passport = require('./config/passportConfig'); 
-var session = require('express-session'); 
+var passport = require('./config/passportConfig');
+var session = require('express-session');
 
 
-// Declare app variable 
+// Declare app variable
 var app = express();
 
 // Set and use statements
 app.set('view engine', 'ejs');
-app.use(ejsLayouts); 
-app.use(bodyParser.urlencoded({ extended: false})); 
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(session({
 	secret: process.env.SESSION_SECRET,
 	resave: false,
-	saveUninitialize: true
+	saveUninitialized: true
 }));
 app.use(flash());
 app.use(passport.initialize());
@@ -37,10 +37,10 @@ app.use('/auth', require('./controllers/auth'));
 app.use('/profile', require('./controllers/profile'));
 app.use('/nachos', require('./controllers/nachos'));
 
-// Define routes 
+// Define routes
 app.get('/', function(req, res){
 	res.render('home');
 });
 
 // listen on port 3000
-app.listen(3000); 
+app.listen(3000);
